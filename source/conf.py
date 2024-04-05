20faf16fe649c3c3550d6890f26746b083e57af0
Benjamin Jakimow benjamin.jakimow@geo.hu-berlin.de <benjamin.jakimow@geo.hu-berlin.de>
2024-04-05 13:36:58 +0200

    updated EnMAP-Box logo
    Signed-off-by: Benjamin Jakimow benjamin.jakimow@geo.hu-berlin.de <benjamin.jakimow@geo.hu-berlin.de>

--- source/conf.py
+++ source/conf.py
@@ -69,7 +69,7 @@ master_doc = 'index'
 # config.read(REPO_ROOT / '.plugin.ini')
 
 # The full version, including alpha/beta/rc tags.
-release = '3.10.3.20220824T155109'
+release = '3.14'
 
 # The short X.Y version.
 version = u'{}'.format(re.search(r'(\.?[^.]*){2}', release).group())
@@ -137,7 +137,7 @@ html_theme_options = {
     'titles_only': False
 }
 
-html_logo = 'img/EnMAP-Box_logo_black_text.svg'
+html_logo = 'img/logos/logo_enmapbox.svg'
 html_favicon = 'img/icon.png'
 
 # Add any paths that contain custom static files (such as style sheets) here,
